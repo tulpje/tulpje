ebd3981a2f23399fa53e230bbe5648cb1124eadc
Zowie <git@zowie.cx>
2026-04-01 13:06:06 +0200

    feat(tulpje-mod-pluralkit): implement role limit handling in `/pk role setup`

--- crates/tulpje-mod-pluralkit/src/roles/shared.rs
+++ crates/tulpje-mod-pluralkit/src/roles/shared.rs
@@ -1,5 +1,13 @@
+use pkrs_fork::{
+    client::{PkClient, PluralKitError},
+    model::{Member, PkId},
+};
+use tulpje_framework::Error;
+use tulpje_lib::{context::CommandContext, responses};
 use twilight_model::channel::message::Component;
 use twilight_util::builder::message::TextDisplayBuilder;
+
+use crate::util::SystemRef;
 
 use super::settings::Settings;
 
@@ -19,4 +27,44 @@ pub(super) fn settings_display(settings: &Settings) -> Vec<Component> {
         .build()
         .into(),
     ]
+}
+
+pub(super) async fn handle_get_system_members(
+    ctx: &CommandContext,
+    client: &PkClient,
+    system_ref: &SystemRef,
+) -> Result<Option<Vec<Member>>, Error> {
+    match client
+        .get_system_members(&PkId(system_ref.clone().into()))
+        .await
+    {
+        Ok(members) => Ok(Some(members)),
+        // private member list
+        Err(PluralKitError::Pk(_, message))
+            // 30001 = unauthorized to view member list
+            if message.code == 30001 =>
+        {
+            // TODO: Try to fetch system name?
+            responses::error(
+                ctx,
+                &format!("### Error\nMember list for `{system_ref}` is private"),
+            )
+            .await?;
+            Ok(None)
+        }
+        // missing system
+        Err(PluralKitError::Pk(_, message))
+            // 20001 = missing system
+            if message.code == 20001 =>
+        {
+            responses::error(
+                    ctx,
+                    &format!("### Error\nPluralKit can't find this system, does `{system_ref}` exist?"),
+                )
+                .await?;
+            Ok(None)
+        }
+        // miscellaneous errors
+        Err(err) => Err(err.into()),
+    }
 }
