bac9f01be6bcb966a6549502981ca60356b1cd72
Zowie <git@zowie.cx>
2026-04-04 13:46:35 +0200

    refactor(tulpje-framework): split internal error handling into separate function

--- crates/tulpje-framework/src/handler/command_handler.rs
+++ crates/tulpje-framework/src/handler/command_handler.rs
@@ -18,41 +18,49 @@ pub struct CommandHandler<T: Clone + Send + Sync> {
 }
 
 impl<T: Clone + Send + Sync> CommandHandler<T> {
+    async fn handle_internal_error(&self, ctx: CommandContext<T>, err: Error) -> Result<(), Error> {
+        tracing::error!(
+            "error during command {}, sending reference to client: {}",
+            self.name,
+            err
+        );
+
+        // TODO: Implement for DMs
+        let Some(chan) = &ctx.event.channel else {
+            tracing::warn!(event = ?ctx.event, "channel on event was empty, can't send error");
+            return Ok(());
+        };
+
+        ctx.client
+            .create_message(chan.id)
+            .flags(MessageFlags::IS_COMPONENTS_V2)
+            .components(&[ContainerBuilder::new()
+                .accent_color(Some(*color::roles::RED))
+                .component(
+                    // TODO: Better way to handle extra error info than, whatever this is
+                    TextDisplayBuilder::new(format!(
+                        "### Internal Error\n{}\n**Error Code**\n```{}```",
+                        std::env::var("TULPJE_EXTRA_ERROR_MESSAGE").unwrap_or_default(),
+                        ctx.meta.uuid
+                    ))
+                    .build(),
+                )
+                .build()
+                .into()])
+            .await?;
+
+        Ok(())
+    }
+
     #[tracing::instrument(name="command-handler", skip_all, fields(
         module=self.module,
         name=self.name
     ))]
     pub async fn run(&self, ctx: CommandContext<T>) -> Result<(), Error> {
-        // TODO: More elegant way of handling command errors
-        // TODO: Test if errors work in DMs
         if let Err(err) = (self.func)(ctx.clone()).await {
-            tracing::error!(
-                "error during command {}, sending reference to client: {}",
-                self.name,
-                err
-            );
-
-            if let Some(chan) = &ctx.event.channel {
-                ctx.client
-                    .create_message(chan.id)
-                    .flags(MessageFlags::IS_COMPONENTS_V2)
-                    .components(&[ContainerBuilder::new()
-                        .accent_color(Some(*color::roles::RED))
-                        .component(
-                            // TODO: Better way to handle extra error info than, whatever this is
-                            TextDisplayBuilder::new(format!(
-                                "### Internal Error\n{}\n**Error Code**\n```{}```",
-                                std::env::var("TULPJE_EXTRA_ERROR_MESSAGE").unwrap_or_default(),
-                                ctx.meta.uuid
-                            ))
-                            .build(),
-                        )
-                        .build()
-                        .into()])
-                    .await?;
-            } else {
-                tracing::warn!(event = ?ctx.event, "channel on event was empty, can't send error");
-            }
+            self.handle_internal_error(ctx, err)
+                .await
+                .map_err(|err| format!("error handling internal error: {err}"))?;
         }
 
         Ok(())
