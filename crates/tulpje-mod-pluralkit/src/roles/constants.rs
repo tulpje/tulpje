0ce2b729020a20df560b0fcce7b796e4ff457a95
Zowie <git@zowie.cx>
2026-04-06 17:59:52 +0200

    feat(tulpje-mod-pluralkit/roles/setup): implement member name length checking

--- crates/tulpje-mod-pluralkit/src/roles/constants.rs
+++ crates/tulpje-mod-pluralkit/src/roles/constants.rs
@@ -1,4 +1,5 @@
 // Discord's role limit
 // see https://support.discord.com/hc/en-us/articles/33694251638295-Discord-Account-Caps-Server-Caps-and-More
 pub(crate) const DISCORD_ROLE_LIMIT: usize = 250;
+pub(crate) const DISCORD_MAX_ROLE_NAME_LENGTH: usize = 100;
 pub(crate) const REMAINING_ROLE_WARNING: usize = 10;
--- crates/tulpje-mod-pluralkit/src/util.rs
+++ crates/tulpje-mod-pluralkit/src/util.rs
@@ -3,6 +3,7 @@ use std::{fmt::Display, str::FromStr};
 use pkrs_fork::model::Member;
 use tulpje_framework::{Error, color};
 use twilight_model::id::{Id, marker::UserMarker};
+use unicode_segmentation::UnicodeSegmentation as _;
 use uuid::Uuid;
 
 use tulpje_lib::{context::CommandContext, responses};
@@ -105,6 +106,45 @@ pub(crate) fn pk_color_to_discord(hex: Option<String>) -> u32 {
     .0
 }
 
+// discord text length limits seem to be based on UTF-16 code units
+// this means that we have to ellipsize based on that.
+// however we also want to not break up any grapheme clusters so we have to handle that too
+pub(crate) fn discord_ellipsize<'a>(
+    text: &str,
+    max_length: usize, // length in `char`s
+    ellipsis_text: impl Into<Option<&'a str>>,
+) -> String {
+    // calculate length in utf16 code units
+    let text_len = text.encode_utf16().count();
+
+    // return full text if it's below limit
+    if text_len <= max_length {
+        return String::from(text);
+    }
+
+    // default option for ellipsis text
+    let ellipsis_text = ellipsis_text.into().unwrap_or("...");
+
+    // calculate ellipsis lenggth in utf16 code units
+    let ellipsis_len = ellipsis_text.encode_utf16().count();
+
+    // fill up a vec with graphemes, stop before it goes past `max_length`
+    let mut total_len = 0;
+    let mut graphemes = Vec::new();
+    for grapheme in text.graphemes(true) {
+        let grapheme_len = grapheme.encode_utf16().count();
+        if total_len + grapheme_len > max_length - ellipsis_len {
+            break;
+        }
+
+        graphemes.push(grapheme);
+        total_len += grapheme_len;
+    }
+
+    let shortened_text = graphemes.join("");
+    format!("{shortened_text}{ellipsis_text}")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -116,5 +156,28 @@ mod tests {
             color::roles::DEFAULT.0
         );
         assert_eq!(pk_color_to_discord(None), color::roles::DEFAULT.0);
+    }
+
+    #[test]
+    fn test_graphene_ellipsize() {
+        // doesn't shorten strings shorter than max length
+        assert_eq!(
+            discord_ellipsize("lorem ipsum, uwu", 100, None),
+            "lorem ipsum, uwu"
+        );
+
+        // shortens strings longer than max length
+        assert_eq!(discord_ellipsize("0123456789", 5, None), "01...");
+
+        // handles custom ellipsis text
+        assert_eq!(discord_ellipsize("0123456789", 5, "…"), "0123…");
+
+        // handles unicode shenanigans (each trans flag is 5 characters)
+        assert_eq!(discord_ellipsize("🏳️‍⚧️🏳️‍⚧️", 9, None), "🏳️‍⚧️...");
+
+        assert_eq!(
+            discord_ellipsize("🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️", 100, ""),
+            "🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️"
+        );
     }
 }
