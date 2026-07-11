f226047b077a1b11de6742d7274322fd6aa8c652
Zowie <git@zowie.cx>
2026-07-11 19:37:28 +0200

    chore: update to rust 1.97.0

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
           # create the toolchain from `rust-toolchain.toml`
           toolchain = inputs.fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
-            sha256 = "sha256-qqF33vNuAdU5vua96VKVIwuc43j4EFeEXbjQ6+l4mO4=";
+            sha256 = "sha256-OATSZm98Es5kIFuqaba+UvkQtFsVgJEBMmS+t6od5/U=";
           };
           # initialise crane with custom toolchain
           craneLib = ((inputs.crane.mkLib pkgs).overrideToolchain (_: toolchain));
